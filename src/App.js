883c445f9bbf03c6c016d572ecbc1c891d40ada6
Svdbroek <s_in_the_water@hotmail.com>
2019-11-18 10:57:44 +0100

    setup complete

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Heavy Metal Universe!
         </a>
       </header>
     </div>
